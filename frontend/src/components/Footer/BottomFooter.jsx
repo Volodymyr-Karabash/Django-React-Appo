b24d2ee7ecd2939a673039db52d76750d353cab0
Volodymyr-Karabash <voca480@gmail.com>
2025-03-25 17:12:13 +0200

    components/Footer/BottomFooter.js added

---
+++ frontend/src/components/Footer/BottomFooter.jsx
@@ -0,0 +1,8 @@
+
+export default function BottomFooter() {
+    return (
+        <section className="footer_section text-center text-white bg-dark py-2 align-content-center ">
+           <p>Appointment Book System  &copy; {new Date().getFullYear()} All Right Reserved </p>
+        </section>
+    )
+}
