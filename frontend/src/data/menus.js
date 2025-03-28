47562fc8a2c8c51af82ff144aeb9859be795a0b6
Volodymyr-Karabash <voca480@gmail.com>
2025-03-28 19:37:27 +0200

    data/menus.js

---
+++ frontend/src/data/menus.js
@@ -0,0 +1,23 @@
+export const menus = [
+    {
+        id: 1,
+        name: "Home",
+        path: "/"
+    },
+    {
+        id: 2,
+        name: "Find a Doctor",
+        path: "/find-doctor"
+    },
+    {
+        id: 3,
+        name: "About Us",
+        path: "/about"
+    },
+    {
+        id: 4,
+        name: "Contact us",
+        path: "/contact"
+    },
+    
+]
