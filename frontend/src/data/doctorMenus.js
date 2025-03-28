df8c60b7b452fe430e69988504bcfb0240f708f6
Volodymyr-Karabash <voca480@gmail.com>
2025-03-28 02:38:04 +0200

    src/data/doctormenus.js added

---
+++ frontend/src/data/doctorMenus.js
@@ -0,0 +1,26 @@
+export const doctorMenus = [
+    {
+        id: 1,
+        name: "Dashboard",
+        path: "doctor-dashboard",
+        icon: "bi bi-speedometer2",
+    },
+    {
+        id: 2,
+        name: "Profile",
+        path: "doctor-profile",
+        icon: "bi bi-person",
+    },
+    {
+        id: 3,
+        name: "appointments",
+        path: "doctor-appointments",
+        icon: "bi bi-calendar3",
+    },
+    // {
+    //     id: 4,
+    //     name: "Patients",
+    //     path: "patients",
+    //     icon: "bi bi-person-plus",
+    // },
+]
