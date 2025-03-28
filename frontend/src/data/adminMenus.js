d6816d0c44e11db7b697c0312969c5f49fd9f3a3
Volodymyr-Karabash <voca480@gmail.com>
2025-03-28 02:36:32 +0200

    src/data/adminMenus.js added

---
+++ frontend/src/data/adminMenus.js
@@ -0,0 +1,27 @@
+
+export const adminMenus = [
+    {
+        id:1,
+        name: "Dashboard",
+        path: "admin-dashboard",
+        icon: "bi bi-speedometer2",
+    },
+    {
+        id: 1,
+        name: "Profile",
+        // path: `admin-profile`,
+        icon: "bi bi-house",
+    },
+    {
+        id: 2,
+        name: "Users",
+        path: "user-list",
+        icon: "bi bi-person",
+    },
+    {
+        id: 3,
+        name: "Doctors",
+        path: "doctor-list",
+        icon: "bi bi-person",
+    },
+]
