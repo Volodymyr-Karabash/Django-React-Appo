ffb63d89da64a708634a6b3756034047f39bdef8
Volodymyr-Karabash <voca480@gmail.com>
2025-03-28 02:37:35 +0200

    src/data/doctorlist.js added

---
+++ frontend/src/data/doctorList.js
@@ -0,0 +1,55 @@
+const doctorList = [
+    {
+        id: 1,
+        name: 'Dr. John Doe',
+        speciality: 'General Physician',
+        location: 'New York, NY',
+        experience: '10 Years',
+        rating: 5,
+        reviews: 100,
+        image: 'https://randomuser.me/api/portraits/men/1.jpg '
+    },
+    {
+        id: 2,
+        name: 'Dr. Jane Doe',
+        speciality: 'Dermatologist',
+        location: 'Los Angeles, CA',
+        experience: '15 Years',
+        rating: 4.5,
+        reviews: 200,
+        image: 'https://randomuser.me/api/portraits/men/2.jpg '
+    },
+    {
+        id: 3,
+        name: 'Dr. Mary Doe',
+        speciality: 'Cardiologist',
+        location: 'Chicago, IL',
+        experience: '20 Years',
+        rating: 4.8,
+        reviews: 150,
+        image: 'https://randomuser.me/api/portraits/men/3.jpg '
+    },
+    {
+        id: 4,
+        name: 'Dr. Smith Doe',
+        speciality: 'Pediatrician',
+        location: 'Houston, TX',
+        experience: '12 Years',
+        rating: 4.7,
+        reviews: 180,
+        image: 'https://randomuser.me/api/portraits/men/4.jpg '
+    },
+    {
+        id: 5,
+        name: 'Dr. Sarah Doe',
+        speciality: 'Gynecologist',
+        location: 'Phoenix, AZ',
+        experience: '18 Years',
+        rating: 4.9,
+        reviews: 250,
+        image: 'https://randomuser.me/api/portraits/men/5.jpg '
+    }
+
+
+
+]
