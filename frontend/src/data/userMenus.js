dcc5caefbe2f57306466e994e97476c9b0d723bc
Volodymyr-Karabash <voca480@gmail.com>
2025-03-28 19:39:17 +0200

    data/userMenus.js

---
+++ frontend/src/data/userMenus.js
@@ -0,0 +1,22 @@
+
+export const userMenus = [
+    {
+        id: 1,
+        name: "Profile",
+        // path: "user-profile",
+        icon: "bi bi-person",
+    },
+    {
+        id: 2,
+        name: "appointments",
+        path: "user-appointments",
+        icon: "bi bi-calendar3",
+    },
+    {
+        id: 3,
+        name: "Apply for Doctor",
+        path: "apply-doctor",
+        icon: "bi bi-person-plus",
+    },
+
+]
